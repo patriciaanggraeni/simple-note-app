d6cfd5fbb5bdc35ea6ade05627600e6a9100e034
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-07-27 15:24:06 +0700

    menambahkan repository dan viewmodel

---
+++ app/src/main/java/com/example/simple_note_app/repository/NoteRepository.kt
@@ -0,0 +1,16 @@
+package com.example.simple_note_app.repository
+
+import androidx.lifecycle.LiveData
+import com.example.simple_note_app.helper.NoteDao
+import com.example.simple_note_app.model.Note
+
+class NoteRepository(
+    private val noteDao: NoteDao
+) {
+
+    fun read(): LiveData<List<Note>> = this.noteDao.read()
+    suspend fun insert(note: Note) = this.noteDao.insert(note)
+    suspend fun update(note: Note) = this.noteDao.update(note)
+    suspend fun delete(note: Note) = this.noteDao.delete(note)
+
+}
---
+++ app/src/main/java/com/example/simple_note_app/view/NoteViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.simple_note_app.view
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.simple_note_app.helper.NoteDatabase
+import com.example.simple_note_app.model.Note
+import com.example.simple_note_app.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application): AndroidViewModel(application) {
+
+    private val fetchNote: LiveData<List<Note>>
+    private val noteRepository: NoteRepository
+
+    init {
+        val noteDao = NoteDatabase(application).noteDao()
+        noteRepository = NoteRepository(noteDao)
+        fetchNote = noteRepository.read()
+    }
+
+    fun readNote() = fetchNote
+
+    fun insertNote(note: Note) {
+        viewModelScope.launch(Dispatchers.IO) {
+            noteRepository.insert(note)
+        }
+    }
+
+    fun updateNote(note: Note) {
+        viewModelScope.launch(Dispatchers.IO) {
+            noteRepository.update(note)
+        }
+    }
+
+    fun deleteNote(note: Note) {
+        viewModelScope.launch(Dispatchers.IO) {
+            noteRepository.delete(note)
+        }
+    }
+
+}
